aacfcae6a8f76697ff9016dfed26e41e4e040fe9
Horloge-Skynet <sam.forestier@gmail.com>
2015-04-10 22:22:32 +0200

    Create example.ino

---
+++ example.ino
@@ -0,0 +1,63 @@
+#include "LCD4884.h"
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+#define NUM_MENU_ITEM  4
+
+char menuList[NUM_MENU_ITEM][NBCHAR_X] = {
+"fct1",
+"fct2",
+"fct3",
+"fct4"};
+
+void fct1(void);
+void fct2(void);
+void fct3(void);
+void fct4(void);
+FONCTION menuFunction[NUM_MENU_ITEM] = {fct1, fct2, fct3, fct4};
+
+char *projectName = "Uniwav:";
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+void setup()
+{
+	lcd.initClass(projectName, NUM_MENU_ITEM);
+	lcd.showMenu(menuList);
+}
+
+
+void loop()
+{
+	lcd.browseMenu(menuList, menuFunction);
+}
+
+
+
+
+void fct1(void)
+{
+	/* */
+}
+
+
+void fct2(void)
+{
+	/* */
+}
+
+
+void fct3(void)
+{
+	/* */
+}
+
+
+void fct4(void)
+{
+	/* */
+}
